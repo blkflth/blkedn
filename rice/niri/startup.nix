6e73e51f1f9ed9fcd6d5c279f8dd18ed6b637d8a
blkflth <crainejasmine@proton.me>
2025-10-28 16:55:32 -0400

    Ricing

--- rice/niri/startup.nix
+++ rice/niri/startup.nix
@@ -1,11 +1,12 @@
 { config, ... }:
 # Start the following programs at launch
 {
-    programs.niri.settings.spawn-at-startup = {
-    "vesktop";
-  # "proton-mail";
-    "firefox";
-    "btm";
-    "microfetch";
-    };
+
+    programs.niri.settings.spawn-at-startup = [
+    { argv = ["vesktop"]; }
+    { argv = ["firefox"]; }
+    { argv = ["btm"]; }
+    { argv = ["microfetch"]; }
+    ];
+
 }
