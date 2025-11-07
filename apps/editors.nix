9f9aee2aafc5b16e5d274271c52f4a5d34489a01
blkflth <crainejasmine@proton.me>
2025-11-07 18:32:14 -0500

    Re-Enabled VSCodium

--- apps/editors.nix
+++ apps/editors.nix
@@ -11,10 +11,16 @@
       package = pkgs.vscodium-fhs;
       profiles.default.extensions = with pkgs.vscode-extensions; [
         vscodevim.vim
+        ms-python.python
         jnoortheen.nix-ide
         rust-lang.rust-analyzer
+        timonwong.shellcheck
         vscode-icons-team.vscode-icons
         sumneko.lua
+        yzhang.markdown-all-in-one
+        yzane.markdown-pdf
+        tomoki1207.pdf
+        
       ];
 
       
