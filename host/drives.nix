a912a84cfc7d26806e29e1618657c7b283e8151d
blkflth <crainejasmine@proton.me>
2025-11-13 20:29:13 -0500

    Reformatted drives as ext4

--- host/drives.nix
+++ host/drives.nix
@@ -6,21 +6,21 @@
   ...
 }: {
   # Mount Points for SSDs
-  fileSystems."/run/media/jlc/SSD One" = {
-    device = "/dev/disk/by-uuid/0A4A387B4A38661B";
-    fsType = "ntfs3";
+  fileSystems."/home/jlc/SSD1" = {
+    device = "/dev/disk/by-uuid/97f253c7-bac6-43d3-b8a9-cc0903c15b7f";
+    fsType = "ext4";
     options = ["nofail" "defaults"];
   };
 
-  fileSystems."/run/media/jlc/SSD Two" = {
-    device = "/dev/disk/by-uuid/7ABA5750BA570851";
-    fsType = "ntfs3";
+  fileSystems."/home/jlc/SSD2" = {
+    device = "/dev/disk/by-uuid/10690921-7698-4027-a3c2-2df47ceaa7d7";
+    fsType = "ext4";
     options = ["nofail" "defaults"];
   };
 
-  fileSystems."/run/media/jlc/SSD Three" = {
-    device = "/dev/disk/by-uuid/027CB1E67CB1D49F";
-    fsType = "ntfs3";
+  fileSystems."/home/jlc/SSD3" = {
+    device = "/dev/disk/by-uuid/659ff393-6687-4c0f-becc-dae68e26f986";
+    fsType = "ext4";
     options = ["nofail" "defaults"];
   };
 }
