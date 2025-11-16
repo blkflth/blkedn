1e1ff16a5276c6c0ac9f5941f661fd966220ad98
blkflth <crainejasmine@proton.me>
2025-11-16 18:21:53 -0500

    removed async prompt in favor of tide

--- apps/fish.nix
+++ apps/fish.nix
@@ -4,13 +4,12 @@
     fzf
     fishPlugins.tide #prompt
     fishPlugins.sponge #filters out typos
-    #fishPlugins.z #directory navigation
+    fishPlugins.z #directory navigation
     fishPlugins.puffer #text expansions
     fishPlugins.plugin-sudope #input ``sudo`` with ``esc+esc``
     fishPlugins.fzf-fish # fzf keybindings
     fishPlugins.fish-you-should-use #alias reminder
     fishPlugins.fish-bd #command to go up in directory
     fishPlugins.colored-man-pages #does what it says
-    fishPlugins.async-prompt #makes the prompt faster
   ];
 }
