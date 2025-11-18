613050357c4acb9e72df387c63e6b6ccd6819f5b
blkflth <crainejasmine@proton.me>
2025-11-18 18:23:05 -0500

    prepared waybar config ahead of disabling noctalia's bar

--- rice/waybar.nix
+++ rice/waybar.nix
@@ -1,6 +1,23 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  config,
+  inputs,
+  ...
+}: let
+  noctalia = cmd:
+    [
+      "noctalia-shell"
+      "ipc"
+      "call"
+    ]
+    ++ (pkgs.lib.splitString " " cmd);
+in {
   imports = [
     ./waybar/waybarstyle.nix
     ./waybar/waybarsettings.nix
   ];
+  programs.waybar = {
+    enable = true;
+  };
 }
--- rice/waybar/waybarsettings.nix
+++ rice/waybar/waybarsettings.nix
@@ -1,26 +1,90 @@
 {
-settings = {
-        mainbar = {
-          layer = "top";
-          position = "top";
-          mod = "dock";
-          exclusive = true;
-          passthrough = false;
-          gtk-layer-shell = true;
-          height = 0;
-          modules-left = [ "clock" ];
-          modules-center = [];
-          modules-right = [
-            "tray"
-          ];
-          tray = {
-            icon-size = 13;
-            spacing = 10;
+  programs.waybar = {
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        mod = "dock";
+        exclusive = true;
+        passthrough = false;
+        gtk-layer-shell = true;
+        height = 0;
+        modules-left = [
+          "custom/nix"
+          "niri/workspaces"
+          "niri/window"
+        ];
+        modules-center = [
+          "niri/language"
+          "clock"
+        ];
+        modules-right = [
+          "disk"
+          "cpu"
+          "memory"
+          "bluetooth"
+          "network"
+          "privacy"
+          "wireplumber"
+        ];
+
+        "disk" = {
+        };
+        "cpu" = {
+        };
+        "memory" = {
+        };
+        "bluetooth" = {
+        };
+        "network" = {
+        };
+        "privacy" = {
+        };
+        "wireplumber" = {
+        };
+
+        "custom/nix" = {
+          format = "󱄅";
+          on-click = noctalia "controlCenter toggle";
+          tooltip = false;
+        };
+
+        "niri/workspace" = {
+          format = "value";
+          disable-click = true;
+        };
+        "niri/window" = {
+          format = "";
+        };
+        "niri/lnaguage" = {
+          format = "Lang: {}";
+          format-en = "EN";
+          format-jp = "JP";
+        };
+        clock = {
+          format = "{%I:Mp%a%d}";
+          timezones = ["America/New_York" "Asia/Tokyo" "Europe/London"];
+          tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+          calendar = {
+            mode = "year";
+            mode-mon-col = "3";
+            weeks-pos = "right";
+            on-scroll = "1";
+            format = {
+              months = "<span color='#ffead3'><b>{}</b></span>";
+              days = "<span color='#ecc6d9'><b>{}</b></span>";
+              weeks = "<span color='#99ffdd'><b>W{}</b></span>";
+              weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+              today = "<span color='#ff6699'><b><u>{}</u></b></span>";
+            };
           };
-          clock = {
-            format = " {:%R   %d/%m}";
-            tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+          actions = {
+            on-click-right = "mode";
+            on-scroll-up = ["tz_up" "shift_up"];
+            on-scroll-down = ["tz_down" "shift_down"];
           };
         };
       };
+    };
+  };
 }
