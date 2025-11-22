e8498ddcf5bacfec23ea80560b0d37fe7b5ebf27
blkflth <crainejasmine@proton.me>
2025-11-22 11:37:21 -0500

    enabled ``feh`` as an image viewer

--- apps/apps.nix
+++ apps/apps.nix
@@ -1,10 +1,11 @@
 {lib, ...}: {
   imports = [
     ./web.nix
-    #./zsh.nix
     ./fish.nix
     ./editors.nix
     ./ghostty.nix
     ./tui.nix
   ];
+
+  programs.feh.enable = true; #image viewer
 }
