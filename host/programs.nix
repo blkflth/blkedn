81bdc676d7f4f67d31533e0f794667e4510e4428
blkflth <crainejasmine@proton.me>
2025-11-23 17:57:11 -0500

    added Garnix to substituters for Affinity

--- host/programs.nix
+++ host/programs.nix
@@ -155,11 +155,13 @@ in {
     substituters = [
       "https://vicinae.cachix.org"
       "https://ezkea.cachix.org"
+      "https://cache.garnix.io"
     ];
 
     trusted-public-keys = [
       "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
       "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
   };
 }
