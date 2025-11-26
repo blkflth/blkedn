27347da8542dc0fc76180fd6c8df5c0ecfb3d115
blkflth <crainejasmine@proton.me>
2025-11-26 00:12:55 -0500

    Package Move

---
+++ host/priv/pin.nix
@@ -0,0 +1,9 @@
+{
+  "version" = "1.34.2";
+  # "revision" = "development";
+  "hashes" = {
+    "desktopSrcHash" = "sha256-GkNHUiRSMfgJGoBiQic3edQPxP/zF9ZWO97iaYNGwZg=";
+    "desktopPkgRootHash" = "sha256-XOeW7fj2UT9fmgYpnF+SguuTAhstdY9rYsOJ/rpNN1g=";
+    "desktopPkgAppHash" = "sha256-74awcILuNxnfx6jc7IXG62MMgf9eMwcgbmHggXdg+YU=";
+  };
+}
