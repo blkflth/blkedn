5c17a37664f73a8138b01a88db544084cdc3aeff
blkflth <crainejasmine@proton.me>
2025-12-21 12:22:44 -0500

    mounted NAS to filesystem

--- host/drives.nix
+++ host/drives.nix
@@ -23,4 +23,15 @@
     fsType = "ext4";
     options = ["nofail" "defaults"];
   };
+
+  # Mount Point for NAS
+  environment.systemPackages = [pkgs.cifs-utils];
+  fileSystems."/home/jlc/NAS" = {
+    device = "//192.168.0.107/nas-share";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets" "nofail"];
+  };
 }
