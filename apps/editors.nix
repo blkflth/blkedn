11809ca74f8cfe4a4b4e5f82fcacaf7be4986afc
blkflth <crainejasmine@proton.me>
2025-12-29 19:43:59 -0500

    Added nvim lazyload settings

--- apps/editors.nix
+++ apps/editors.nix
@@ -22,6 +22,26 @@
       blink-cmp-git
       monokai-pro-nvim
     ];
+    /*
+    extraLuaConfig = ''
+      vim.g.mapleader = " " -- Need to set leader before lazy for correct keybindings
+      require("lazy").setup({
+        performance = {
+          reset_packpath = false,
+          rtp = {
+              reset = false,
+            }
+          },
+        dev = {
+          path = "${pkgs.vimUtils.packDir config.home-manager.users.jlc.programs.neovim.finalPackage.passthru.packpathDirs}/pack/myNeovimPackages/start",
+        },
+        install = {
+          -- Safeguard in case we forget to install a plugin with Nix
+          missing = false,
+        },
+      })
+    '';
+    */
   };
 
   programs.vscode = {
