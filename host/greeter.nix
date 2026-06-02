9c7a39228a81b21db4f54462b632e2b0224b9ea8
Jas <crainejasmine@proton.me>
2026-06-02 16:19:46 -0400

    added config for regreet

--- host/greeter.nix
+++ host/greeter.nix
@@ -1,10 +1,29 @@
 {pkgs, ...}: {
+  programs.regreet = {
+    enable = true;
+    font = {
+      name = "AtkynsonMono NFP";
+      package = pkgs.nerd-fonts.atkynson-mono;
+      size = 12;
+    };
+    iconTheme = {
+      name = "Dracula";
+      package = pkgs.dracula-icon-theme;
+    };
+    cursorTheme = {
+      name = "catppuccin-frappe-dark-cursors";
+      package = pkgs.catppuccin-cursors.frappeDark;
+    };
+  };
+
   services.greetd = {
     enable = true;
     useTextGreeter = true;
     settings = {
       default_session = {
-        command = ''
+        command = "dbus-run-session niri-session --config /home/jlc/Nix//host/nirigreet.kdl";
+        /*
+            ''
           ${pkgs.tuigreet}/bin/tuigreet \
             --time \
             --asterisks \
@@ -14,6 +33,7 @@
             --theme text=red;prompt=green;time=red;input=red;border=white;title=red;action=white;greet=white \
             --cmd niri-session
         '';
+        */
         user = "greeter";
       };
     };
